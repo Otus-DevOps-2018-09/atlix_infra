c847f1887e0f66b18bf4f9316c31a5c980a5dbc5
Vladimir Stafievsky <justdewil@gmail.com>
2018-11-11 18:09:56 +0300

    Tests added

--- play-travis/install_deps.sh
+++ play-travis/install_deps.sh
@@ -3,13 +3,13 @@
 set -e
 
 echo "### Installing dependencies for tests ###"
-sudo pip install ansible-lint==3.5.1
+sudo pip -q install ansible-lint==3.5.1
 
 cd /tmp
 
-wget https://releases.hashicorp.com/packer/1.3.2/packer_1.3.2_linux_amd64.zip && sudo unzip packer_1.3.2_linux_amd64.zip -d /usr/sbin
-wget https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip sudo && unzip terraform_0.11.10_linux_amd64.zip -d /usr/sbin
-wget https://github.com/wata727/tflint/releases/download/v0.7.2/tflint_linux_amd64.zip && sudo unzip tflint_linux_amd64.zip -d /usr/sbin
+wget -q https://releases.hashicorp.com/packer/1.3.2/packer_1.3.2_linux_amd64.zip && unzip packer_1.3.2_linux_amd64.zip -d /usr/sbin
+wget -q https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip  && unzip terraform_0.11.10_linux_amd64.zip -d /usr/sbin
+wget -q https://github.com/wata727/tflint/releases/download/v0.7.2/tflint_linux_amd64.zip && unzip tflint_linux_amd64.zip -d /usr/sbin
 
 rm -f *.zip
 
